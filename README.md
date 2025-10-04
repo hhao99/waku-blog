e58851cf20c4df45c19705482ead2ceb907d2d6b
hhao99 <hhao99@gmail.com>
2025-10-04 12:21:38 -0500

    update readme and post list

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 * [Waku dev docs](https://waku.gg)
 * [React 19](https://react.dev/blog/2024/12/05/react-19)
 * [Drizzle ORM](https://orm.drizzle.team/)
+
+# Command
+1. pnpm dev - start the dev server and open localhost:3000 for test
+2. pnpm build - production build
+3. pnpm drizzle-kit generate # generate the database schema
+4. pnpm drizzle-kit migrarte # migrate the database schema
+5. pnpm seed # push the sample data to database
+
--- src/components/posts/item.tsx
+++ src/components/posts/item.tsx
@@ -1,9 +1,12 @@
 import type { Post } from '@/data/schemas/posts'
 export default function PostItem({post}: {post: Post}) {
     return (
-        <div>
+        <div className='flex flex-col w-full border-b-1 border-gray-200 rounded-lg shadow-lg'>
             <div><a href={`/posts/${post.id}`}>{post.title}</a></div>
-            <div>created at: {post.createdAt}</div>     
+            <div>
+                <span className='text-lg text-gray-600 p-2'>{post.description}</span>
+                <span className='text-sm text-gray-500 p-2'>created at: {new Date(post.createdAt).toLocaleString('en-US')}</span>
+            </div>     
         </div>
     )
 }
